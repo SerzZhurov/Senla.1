bfd8dabef8c566f86655d9486a0a2c808e93319b
SerzZhurov <minskgeo@gmail.com>
2021-02-08 14:28:58 +0300

    Работа с массивами 2

--- Arrays_and_Strings/Arr.java
+++ Arrays_and_Strings/Arr.java
@@ -5,49 +5,49 @@ import Learning.Array;
 import java.util.Scanner;
 
 
-    public class Arr {
+public class Arr {
 
-        @Override
-        public String toString() {
-            return "Arr{}";
-        }
-
-        public void square() {
-            Scanner input = new Scanner(System.in);
-            int i = 0;
-            int j = 0;
-            int sc;
-            int randSym;
-            String stringInArray = "";
-            String stringChoice = "";
-            String string = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-            System.out.println("input array size:");
-            int sz = input.nextInt();
-            String SqAr[][] = new String[sz][sz];
-            for (i = 0; i < SqAr.length; i++) {                                                                 //задаем цикл перебора строк
-                for (j = 0; j < SqAr.length; j++) {                                                             //задаем цикл перебора столбцов
-                    for (sc = 0; sc < 6; sc++) {                                                                  //--------------------------
-                        stringChoice = String.valueOf(string.charAt(randSym = (int) (52 * (Math.random()))));     //цикл создания строки длиной 6 с случайными символами
-                        stringInArray = stringInArray + stringChoice;                                           //--------------------------
-                    }
-                    if ((i + j) % 3 != 0) {
-                        SqAr[i][j] = stringInArray;
-                        stringInArray = "";
-                    } else {
-                        double result = 10 * (double) Math.random();
-                        SqAr[i][j] = String.valueOf(result);
-                    }
-                }
-            }
-            System.out.println("Array size " + SqAr.length + " x " + SqAr.length);
-            for (i = 0; i < SqAr.length; i++) {
-                for (j = 0; j < SqAr.length; j++) {
-                    String print = new StringBuilder().append(SqAr[i][j]).toString();
-                    print = print.substring(0, 6);
-                    System.out.print(print + " ");
-                }
-                System.out.println("");
-            }
-            return;
-        }
+    @Override
+    public String toString() {
+        return "Arr{}";
     }
+
+    public void square() {
+        Scanner input = new Scanner(System.in);
+        int i = 0;
+        int j = 0;
+        int sc;
+        int randSym;
+        String stringInArray = "";
+        String stringChoice = "";
+        String string = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+        System.out.println("input array size:");
+        int sz = input.nextInt();
+        String SqAr[][] = new String[sz][sz];
+        for (i = 0; i < SqAr.length; i++) {                                                                 //задаем цикл перебора строк
+            for (j = 0; j < SqAr.length; j++) {                                                             //задаем цикл перебора столбцов
+                for (sc = 0; sc < 6; sc++) {                                                                  //--------------------------
+                    stringChoice = String.valueOf(string.charAt(randSym = (int) (52 * (Math.random()))));     //цикл создания строки длиной 6 с случайными символами
+                    stringInArray = stringInArray + stringChoice;                                           //--------------------------
+                }
+                if ((i + j) % 3 != 0) {
+                    SqAr[i][j] = stringInArray;
+                    stringInArray = "";
+                } else {
+                    double result = 10 * (double) Math.random();
+                    SqAr[i][j] = String.valueOf(result);
+                }
+            }
+        }
+        System.out.println("Array size " + SqAr.length + " x " + SqAr.length);
+        for (i = 0; i < SqAr.length; i++) {
+            for (j = 0; j < SqAr.length; j++) {
+                String print = new StringBuilder().append(SqAr[i][j]).toString();
+                print = print.substring(0, 6);
+                System.out.print(print + " ");
+            }
+            System.out.println("");
+        }
+        return;
+    }
+}
--- Arrays_and_Strings/Main.java
+++ Arrays_and_Strings/Main.java
@@ -17,5 +17,7 @@ public class Main {
     public static void main(String[] args) {
         Arr array = new Arr();
         array.square();
+
+
     }
 }
