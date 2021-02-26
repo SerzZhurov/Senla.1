12903e41302f5d3c9994a981919e80a7d5c655a7
SerzZhurov <minskgeo@gmail.com>
2021-02-26 14:45:36 +0300

    Компаратор

--- List/Main.java
+++ List/Main.java
@@ -1,4 +1,11 @@
 package List;
 
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
 }
--- List/MyArrayList.java
+++ List/MyArrayList.java
@@ -5,59 +5,64 @@ import java.util.Comparator;
 import java.util.ListIterator;
 
 public class MyArrayList<E> implements MyList<E> {
-
-    @Override
     public void add(int index, E obj) {
+
 
     }
 
-    @Override
     public boolean addAll(int index, Collection<? extends E> col) {
         return false;
     }
 
-    @Override
     public E get(int index) {
         return null;
     }
 
-    @Override
     public int indexOf(Object obj) {
         return 0;
     }
 
-    @Override
     public int lastlndexOf(Object obj) {
         return 0;
     }
 
-    @Override
     public ListIterator<E> listiterator() {
         return null;
     }
 
-    @Override
+    public <E1> MyList<E1> of(Object... e) {
+        return null;
+    }
+
     public MyList<E> of() {
         return null;
     }
 
-    @Override
     public E remove(int index) {
         return null;
     }
 
-    @Override
     public E set(int index, E obj) {
         return null;
     }
 
-    @Override
     public void sort(Comparator<? super E> comp) {
 
     }
 
-    @Override
     public MyList<E> subList(int start, int end) {
         return null;
+    }
+
+    public void ArrayList() {
+
+    }
+
+    public void ArrayList(Collection<? extends E> col) {
+
+    }
+
+    public void ArrayList(int capacity) {
+
     }
 }
