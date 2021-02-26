0ecdda12e9d884ae855b55ce21fc98aadfea7481
SerzZhurov <minskgeo@gmail.com>
2021-02-26 16:02:16 +0300

    Компаратор

--- List/Main.java
+++ List/Main.java
@@ -1,11 +1,17 @@
 package List;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-
-
+int i;
+        ArrayList arlTest = new ArrayList();
+        System.out.println("Размер созданного массива на начальном этапе " + arlTest.size());
+        for (i = 0;i<10; i++) {
+            arlTest.add(i);
+        }
+        System.out.println("Размер созданного массива на втором этапе " + arlTest.size());
+        System.out.println("элементы массива" + arlTest);
     }
 }
